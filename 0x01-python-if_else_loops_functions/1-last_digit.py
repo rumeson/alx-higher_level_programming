d98e423e511d850937c5e2accea7c708ecec7ea4
Rume Dolor <doghenerume@gmail.com>
2022-09-28 00:50:04 -0700

    changed to truncated modulo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
-str_num = str(number)
-for num in str_num:
-    if num == str_num[-1]:
-        num = int(num)
-        print("Last digit of {:d} is {:d} and is".format(number, num), end=" ")
-        if num > 5:
-            print("greater than 5")
-        elif num == 0:
-            print(0)
-        elif num < 6 and num != 0:
-            print("less than 6 and not 0")
+last_num = math.fmod(number, 10)
+print("Last digit of {:.0f} is {:.0f} and is".format(number, last_num), end=" ")
+if last_num > 5:
+    print("greater than 5")
+elif last_num == 0:
+    print(0)
+elif last_num < 6 and last_num != 0:
+    print("less than 6 and not 0")
