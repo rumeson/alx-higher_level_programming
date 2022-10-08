687acde145bce24ece9b8142a4d3c3608b3a12b0
Rume Dolor <doghenerume@gmail.com>
2022-10-08 12:27:33 -0700

    edit 8-upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= ord("a") and ord (i) <= ord("z"):
+        if ord(i) >= ord("a") and ord(i) <= ord("z"):
             char = chr(ord(i) - 32)
         else:
             char = i
-        print("{:s}".format(char), end="")                                                                                      
-    print("") 
+        print("{:s}".format(char), end="")
+    print("")
