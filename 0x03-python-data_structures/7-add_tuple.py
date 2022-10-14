3eb7ff646ace3b51491fdf54bdacaf2f020b32f6
Rume Dolor <doghenerume@gmail.com>
2022-10-14 09:14:31 -0700

    9-max_integer.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    copy_a = tuple_a
-    copy_b = tuple_b
-    if copy_a[0] == False:
-        copy_a[0] = 0
-    elif copy_a[1] == False:
-        copy_a[1] = 0
-    elif copy_b[0] == False:
-        copy_b[0] = 0
-    elif copy_b[1] == False:
-        copy_b[1] = 0
-    print(copy_a[0] + copy_b[0], copy_a[1] + copy_b[1])
+    new_tuple = ()
+    tuple1 = tuple_a + (0, 0)
+    tuple2 = tuple_b + (0, 0)
+    new_tuple = tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
+    return new_tuple
