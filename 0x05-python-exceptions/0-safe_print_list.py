a8822989a0585062d8db99ef396500bc07a8f6e6
Rume Dolor <doghenerume@gmail.com>
2022-10-14 21:44:36 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 def safe_print_list(my_list=[], x=0):
     ele_printed = 0
-    try:
-        while ele_printed < x:
+    while ele_printed < x:
+        try:    
             print("{}".format(my_list[ele_printed]), end="")
             ele_printed += 1
-        print()
-        return ele_printed
-    except IndexError:
-        print("")
-        return ele_printed
+        except IndexError:
+            break
+    print()
+    return ele_printed
