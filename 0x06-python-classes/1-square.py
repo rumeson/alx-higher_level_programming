e9d2f1794f9f140b476792bcab447289ee0cbf4c
Rume Dolor <doghenerume@gmail.com>
2022-10-30 13:11:43 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class square with a
+    private instance attribute
+    size"""
+
+
+class Square():
+
+    """A square with private instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
