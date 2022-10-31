06ff916aecb8aeca70f35224b5426a5de5988b19
Rume Dolor <doghenerume@gmail.com>
2022-10-30 22:58:33 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a class that defines a square with
+    private instance attribute size that must be
+    an integer, otherwise raise a TypeErroe exception
+    and if size < 0, raise a ValueError exception"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
